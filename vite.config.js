fa059ea1fc6affce530767c5a267ba811df62daf
Rohit Tyagi <rohit.tyagi@thoughts2binary.com>
2024-05-05 09:36:55 +0530

    init

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import path, { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: "globalThis",
+      },
+    },
+  },
+});
